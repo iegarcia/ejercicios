a2f60787ca03dd60125051e2dec57e63a138cc53
iegarcia <nachogarcia003@gmail.com>
2020-08-05 13:54:16 -0300

    Ejercicios 2 apis hecho!

--- apis/index.js
+++ apis/index.js
@@ -1,4 +1,6 @@
-let div = document.getElementById("ej1");
+/* Ejercicio 1
+-------------------------------------------------------------------------------------------- */
+let paper = document.getElementById("ej1");
 let res1 = document.createElement("h5");
 let full_name;
 let people = [];
@@ -23,10 +25,50 @@ function manipular(datos) {
   res1.innerText =
     "Porcentaje de personas cuyo nombre y apellido tienen mas de 12 caracteres es " +
     porcentaje;
-  div.append(res1);
-  return console.log(porcentaje);
+  paper.append(res1);
+  // return console.log(porcentaje);
 }
 
 function validar(full_name) {
   return full_name.length > MAX_CANT;
+}
+
+/* Ejercicio 2
+-------------------------------------------------------------------------------------------- */
+let ej2 = document.getElementById("ej2");
+let btn = document.getElementById("btn");
+
+fetch("https://reqres.in/api/users")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (ppl) {
+    visualizar(ppl.data);
+  });
+
+function visualizar(info) {
+  for (i of info) {
+    let imagen = document.createElement("img");
+    imagen.setAttribute("src", i.avatar);
+    ej2.append(imagen);
+  }
+}
+function capturar() {
+  let input = document.getElementById("text").value;
+  return mostrar(input);
+}
+
+function mostrar(num) {
+  fetch("https://reqres.in/api/users?page=" + num)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (d) {
+      limpiar();
+      visualizar(d.data);
+    });
+}
+
+function limpiar() {
+  ej2.innerHTML = null;
 }
